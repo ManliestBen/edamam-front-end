cbf0f1293980b49b0722b1cab5bde6d62a93a5ec
Ben Manley <benjamintmanley@gmail.com>
2024-03-11 09:39:28 -0500

    stub up search form and prep for service function

--- src/pages/RecipeSearch/RecipeSearch.jsx
+++ src/pages/RecipeSearch/RecipeSearch.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 const RecipeSearch = () => {
+  const [formData, setFormData] = useState({
+    query: ''
+  })
+  const [results, setResults] = useState([])
+  
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // MAKE API CALL USING STATE
+    // SET RESULTS WITH RETURNED DATA
+  }
+
   return (
     <>
       <h1>Recipe Search</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" onChange={handleChange}/>
+        <button type="submit">Search</button>
+      </form>
     </>
   )
 }
