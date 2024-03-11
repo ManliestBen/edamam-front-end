151f8024c703976b779d7bd9884553ec6374c7d4
Ben Manley <benjamintmanley@gmail.com>
2024-03-11 12:27:15 -0500

    add ingredient toggle display

--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import * as recipeService from '../../services/recipeService'
+import styles from './RecipeDetails.module.css'
 
 
 const RecipeDetails = () => {
   const { edamamId } = useParams()
   const [recipe, setRecipe] = useState({})
+  const [displayIngredients, setDisplayIngredients] = useState(false)
 
   useEffect(() => {
     const fetchRecipeDetails = async () => {
@@ -15,13 +17,29 @@ const RecipeDetails = () => {
     fetchRecipeDetails()
   }, [edamamId])
 
+  const handleToggleIngredientDisplay = () => {
+    setDisplayIngredients(!displayIngredients)
+  }
 
   return (
     <>
       {recipe.uri ?
-        <div>
+        <div className={styles.recipeContainer}>
           <h1>{recipe.label}</h1>
           <img src={recipe.images.REGULAR.url} alt="Image of this recipe" />
+          <a href={recipe.url}>Instructions</a>
+          <h3>Calories: {Math.floor(recipe.calories)}</h3>
+          <h3>Prep Time: {Math.floor(recipe.totalTime)} min</h3>
+          <h3>Feeds: {recipe.yield}</h3>
+          <button onClick={handleToggleIngredientDisplay} className={styles.ingredientDisplay}>{displayIngredients ? 'Hide' : 'Show'} Ingredients</button>
+          {displayIngredients &&
+            <ul>
+              {recipe.ingredientLines.map(ingredient =>
+                <li key={ingredient}>{ingredient}</li>
+              )}
+            </ul>
+          }
+
         </div>
       :
         <h2>Loading...</h2>
